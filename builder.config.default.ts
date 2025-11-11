fc14c4c91a832b044420041b64b6f619fb287c10
longlin10086 <gnulonglin@gmail.com>
2025-11-11 18:15:13 +0800

    chores: customize

--- builder.config.default.ts
+++ builder.config.default.ts
@@ -12,12 +12,12 @@ export default defineBuilderConfig(() => ({
   },
   storage: {
     // "provider": "local",
-    // "basePath": "./apps/web/public/photos",
-    // "baseUrl": "/photos"
+    // "basePath": "/Users/longlin/Pictures/Export",
+    // "baseUrl": "http://localhost:1924",
 
     provider: 's3',
     bucket: env.S3_BUCKET_NAME,
-    region: env.S3_REGION,
+    // region: env.S3_REGION,
     endpoint: env.S3_ENDPOINT,
     accessKeyId: env.S3_ACCESS_KEY_ID,
     secretAccessKey: env.S3_SECRET_ACCESS_KEY,
